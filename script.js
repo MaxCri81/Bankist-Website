7d3a935038256d53c34196c54cecb573843c8d64
Massimiliano Crisafulli <maxone981@gmail.com>
2025-07-25 10:10:03 +0100

    Update script.js
    sectionOneScroll: Scroll the page to section 1 with smooth behaviour

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 'use strict';
-
+/***************************************************************************** Declarations ****************************************************************************/
 // Modal window
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+// Smooth scrolling
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.getElementById("section--1");
 
+/**************************************************************************** Event handlers ************************************************************************/
 // attach the event listener to the 2 buttons 'btn--show-modal'
 btnsOpenModal.forEach(button => button.addEventListener('click', openModal));
 
@@ -16,14 +20,22 @@ btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
 // attach the event listener to the all page for a key pressed
-document.addEventListener('keydown', function (event) {
+document.addEventListener('keydown', closeModalEscape);
+
+// attach the event listener to the Learn more button
+btnScrollTo.addEventListener("click", sectionOneScroll);
+/********************************************************************************* Functions ***************************************************************************/
+/**
+ * When 'Escaped' key is pressed closeModal is called.
+ * @param {Object} event - PointerEvent returned from document event listener 
+ */
+function closeModalEscape(event) {
   // if the key Escape is pressed and the modal window doesn't have the class hidden
   // (meaning the modal window is shown) then the closeModalWindow function is called
   if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
-  }
-});
-
+  };
+};
 /**
  * Open the modal window and shows the overlay class
  * @param {Object} event - PointerEvent returned from btnsOpenModal event listener 
@@ -38,6 +50,11 @@ function openModal(event) {
 function closeModal() {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
+};
+
+/** Scroll the page to section 1 with smooth behaviour */
+function sectionOneScroll() {
+  section1.scrollIntoView({behavior: "smooth"});
 };
 
 
